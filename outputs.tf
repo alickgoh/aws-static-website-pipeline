9a37c7eca06efb613bb6a0429fe79942375ed47f
alickgoh <111750444+alickgoh@users.noreply.github.com>
2025-06-09 10:26:03 +0800

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "website_url" {
-  description = "The URL of the static website"
-  value       = aws_s3_bucket.static_site.website_endpoint
-}
-
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.static_site.id
-}
