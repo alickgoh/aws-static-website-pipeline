848e94b18a41b64a240db9b788d96841272acd36
alickgoh <111750444+alickgoh@users.noreply.github.com>
2025-06-09 10:26:59 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,16 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-gohkl"
-    key            = "static-website/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-locks"
+    # Replace with your bucket name
+    bucket = "my-terraform-state-lock-gohkl" 
+
+    # This is the path to the state file inside the bucket
+    key    = "global/s3/terraform.tfstate"
+
+    # Replace with your bucket's region
+    region = "ap-southeast-1"
+
+    # Enable state locking. This will create a .tflock file in S3.
+    # No DynamoDB table is needed!
+    encrypt        = true
   }
 }
