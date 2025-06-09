5a8ad6ee3330677f0c119faf250a762b24e6d368
alickgoh <111750444+alickgoh@users.noreply.github.com>
2025-06-09 10:29:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,47 +2,52 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "my-terraform-website-gohkl" # Must be globally unique
 
   tags = {
-    Name = "StaticWebsite"
+    Name        = "My Static Website Bucket"
+    Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.static_site.id
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
 
-resource "aws_s3_bucket_policy" "public_policy" {
-  bucket = aws_s3_bucket.static_site.id
-
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.site_bucket.id
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site_bucket.arn}/*"
     }]
   })
 }
 
-resource "null_resource" "upload_site" {
-  provisioner "local-exec" {
-    command = "aws s3 sync site s3://${aws_s3_bucket.static_site.bucket} --delete"
-  }
-
-  depends_on = [aws_s3_bucket_policy.public_policy]
+resource "aws_s3_object" "index_page" {
+  bucket       = aws_s3_bucket.site_bucket.id
+  key          = "index.html"
+  source       = "index.html" # Path to your local file
+  content_type = "text/html"
+  
+  # The etag is used to ensure the file is re-uploaded if it changes
+  etag = filemd5("index.html")
 }
+
+output "website_endpoint" {
+  value = "http://${aws_s3_bucket_website_configuration.site.website_endpoint}"
+}
+
